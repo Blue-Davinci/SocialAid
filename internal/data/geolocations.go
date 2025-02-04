d9c42e0b7263ab685e6b82b2ffd3950c398364a9
DaVinCi <52623427+Blue-Davinci@users.noreply.github.com>
2025-02-04 10:45:28 +0300

    Added capability to create a new GeoLocation.

--- internal/data/geolocations.go
+++ internal/data/geolocations.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"context"
+	"errors"
 	"time"
 
 	"github.com/Blue-Davinci/SocialAid/internal/database"
@@ -10,6 +12,14 @@ import (
 type GeoLocationsManagerModel struct {
 	DB *database.Queries
 }
+
+const (
+	DefaultGeoLocManDBContextTimeout = 5 * time.Second
+)
+
+var (
+	ErrDuplicateGeoLocation = errors.New("geo location already exists, please choose another one")
+)
 
 type GeoLocation struct {
 	ID          int32     `json:"id"`
@@ -31,6 +41,31 @@ func ValidateGeoLocation(v *validator.Validator, g *GeoLocation) {
 	v.Check(len(g.SubLocation) <= 255, "sub_location", "must not be more than 255 bytes long")
 }
 
+// CreateNewGeoLocation() creates a new geo location in the database
+// We recieve a pointer to a GeoLocation struct and return an error if the geo location already exists or
+// if there was an error creating the geo location
 func (m GeoLocationsManagerModel) CreateNewGeoLocation(geoLocation *GeoLocation) error {
+	// create context
+	ctx, cancel := contextGenerator(context.Background(), DefaultGeoLocManDBContextTimeout)
+	defer cancel()
+	// create new geo location
+	geoLocationInfo, err := m.DB.CreateNewGeoLocation(ctx, database.CreateNewGeoLocationParams{
+		County:      geoLocation.County,
+		SubCounty:   geoLocation.SubCounty,
+		Location:    geoLocation.Location,
+		SubLocation: geoLocation.SubLocation,
+	})
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "geolocations_county_key"`:
+			return ErrDuplicateGeoLocation
+		default:
+			return err
+		}
+	}
+	// set the new geo location info
+	geoLocation.ID = geoLocationInfo.ID
+	geoLocation.CreatedAt = geoLocationInfo.CreatedAt
+
 	return nil
 }
